a512d1017155c49389f7f0fe6f5277f95b729d90
Sridesh Fernando <srideshhimalka@gmail.com>
2024-11-12 23:49:49 +0530

    backend

--- server/controllers/personController.js
+++ server/controllers/personController.js
@@ -10,4 +10,32 @@ const listPersons = async (req, res) => {
   }
 };
 
-module.exports = { listPersons };
+const createPerson = async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    date_of_birth,
+    country_of_birth,
+    gender,
+  } = req.body;
+  console.log(date_of_birth, country_of_birth);
+
+  try {
+    const newPerson = await personModel.createPerson(
+      first_name,
+      last_name,
+      email,
+      date_of_birth,
+      country_of_birth,
+      gender
+    );
+    res.status(200).json(newPerson);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ error: "Error creating person" });
+  }
+};
+
+module.exports = { listPersons, createPerson };
